2fb89e978f7179af66f0cf274922cbcba0c4a6e1
Gabriel Agyeman Duah <gabrielagyemanduah@gmail.com>
2026-02-02 13:58:51 +0000

    paystack is added

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://slipper-api-updated.onrender.com/api'
+  apiUrl: 'https://slipper-api-updated.onrender.com/api',
+  paystackPublicKey: 'pk_test_d393bf94c458a0fd728ccb40659eae44c651f268'
 };
